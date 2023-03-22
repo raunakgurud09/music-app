d0cc315efef4ba34e664b3cc5cdce825422e610e
raunak gurud <raunakgurud2121@gmail.com>
2023-03-22 23:39:09 +0530

    feat: track player functional

--- src/components/core/TrackPlayer.tsx
+++ src/components/core/TrackPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import Image from 'next/image';
 import lead from '../Playlist/Lead-image.png';
 // import AudioPlayer from 'react-h5-audio-player';
@@ -17,12 +17,23 @@ import TrackImage from '../ui/Images/TrackImage';
 
 function TrackPlayer() {
   const { player }: any = useContext(PlayerContext);
+  const [play, setPlay] = useState(false);
 
   const audioEle: any = useRef();
+
+  const togglePlay = () => {
+    setPlay(!play);
+    play ? handlePlay() : handlePause();
+  };
 
   const handlePlay = () => {
     if (!audioEle.current) return;
     audioEle.current.play();
+  };
+
+  const handlePause = () => {
+    if (!audioEle.current) return;
+    audioEle.current.pause();
   };
 
   const handleClick = () => {
@@ -52,19 +63,15 @@ function TrackPlayer() {
           </div>
         </div>
         <div>
-          <audio src={player.audioUrl} controls ref={audioEle}></audio>
-          {/* <AudioPlayer
-            src='https://res.cloudinary.com/dmaeznlik/video/upload/v1679085888/audio/6414c596b30e2fca36123639.mp3'
-            onPlay={() => console.log("is playing")}
-            autoPlay={true}
-            showSkipControls={true}
-            // onClickNext={nextTrack}
-            // onClickPrevious={previousTrack}
-          /> */}
+          <audio src={player.audioUrl} ref={audioEle}></audio>
           <div className='mb-6 flex items-center justify-center space-x-4'>
             <AudioBtn icon={<ShuffleIcon />} fn={handleClick} />
             <AudioBtn icon={<PrevIcon />} fn={handleClick} />
-            <AudioBtn icon={<Play />} fn={handlePlay} />
+            {play ? (
+              <AudioBtn icon={<Play />} fn={togglePlay} />
+            ) : (
+              <AudioBtn icon={<PlayIcon />} fn={togglePlay} />
+            )}
             <AudioBtn icon={<NextIcon />} fn={handleClick} />
             <AudioBtn icon={<LoopIcon />} fn={handleClick} />
           </div>
